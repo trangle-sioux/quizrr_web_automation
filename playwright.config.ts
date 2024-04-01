a5adf7f1cec4ddc9ee84623447400c634e6a1045
trangle-sioux <124568331+trangle-sioux@users.noreply.github.com>
2024-04-01 17:21:18 +0700

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: Boolean(process.env.CI),
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* Skip retrive */
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
